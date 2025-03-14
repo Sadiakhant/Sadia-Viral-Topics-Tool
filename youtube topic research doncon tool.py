0d1787d714f4f85fdb1116a59c06a420fd08173d
Sadiakhant <sadiashabeer009@gmail.com>
2025-03-14 21:21:34 +0500

    Update youtube topic research doncon tool.py

--- youtube topic research doncon tool.py
+++ youtube topic research doncon tool.py
@@ -3,7 +3,7 @@ import requests
 from datetime import datetime, timedelta
 
 # YouTube API Key
-API_KEY = "YOUR_YOUTUBE_API_KEY"  # Replace with your valid API Key
+API_KEY = "YOUR_YOUTUBE_API_KEY"  # AIzaSyACH1qjm6FuvEgvgxdA898_dDkMolyYcIM
 YOUTUBE_SEARCH_URL = "https://www.googleapis.com/youtube/v3/search"
 YOUTUBE_VIDEO_URL = "https://www.googleapis.com/youtube/v3/videos"
 YOUTUBE_CHANNEL_URL = "https://www.googleapis.com/youtube/v3/channels"
